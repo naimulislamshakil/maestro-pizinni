530fba962ad9a4e81c32c3767b4b953bd87daa9f
naimulislamshakil <ms0413354@gmail.com>
2022-04-20 19:23:11 +0600

    banner

--- src/Component/Shared/Header/Header.js
+++ src/Component/Shared/Header/Header.js
@@ -1,17 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
   return (
     <div className="banner">
-      <div className="container">
+      <div
+        className="container d-flex align-items-center text-white"
+        style={{ height: "100vh" }}
+      >
         <div className="banner-description">
           <h3>Pizza Delivery</h3>
           <h2>MAESTRO PIZINNI</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
-            porro voluptatem
+            porro voluptatem.
           </p>
+          <Link className="btn btn-danger text-uppercase fw-bold" to="about">
+            delivery now
+          </Link>
         </div>
       </div>
     </div>
