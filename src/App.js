59d50b85cc692aecbd10fcbc0bc65b7016c5435f
naimulislamshakil <ms0413354@gmail.com>
2022-04-20 19:10:03 +0600

    Banner setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Component/Shared/Header/Header";
+import { Route, Routes } from "react-router-dom";
+import Home from "../src/Component/Pages/Home/Home";
+import About from "../src/Component/Pages/About/About";
+import Contact from "../src/Component/Pages/Contact/Contact";
+import Menu from "../src/Component/Pages/Menu/Menu";
+import NotFound from "../src/Component/Pages/NotFound/NotFound";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/menu" element={<Menu></Menu>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/Component/Shared/Header/Header.js
+++ src/Component/Shared/Header/Header.js
@@ -1,21 +1,17 @@
 import React from "react";
-import NavBar from "../NavBar/NavBar";
 import "./Header.css";
 
 const Header = () => {
   return (
     <div className="banner">
-      <NavBar></NavBar>
       <div className="container">
-        <div className="d-flex align-items-center" style={{ height: "100vh" }}>
-          <div className="text-white banner-description">
-            <h3>Pizza Delivery</h3>
-            <h2>MAESTRO PIZINNI</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-              sunt aliquid doloremque similique.
-            </p>
-          </div>
+        <div className="banner-description">
+          <h3>Pizza Delivery</h3>
+          <h2>MAESTRO PIZINNI</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
+            porro voluptatem
+          </p>
         </div>
       </div>
     </div>
